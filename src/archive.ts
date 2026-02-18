f4bf9282779c44d1c7813a073b901b57a351e1fd
William Chong <6198816+williamchong@users.noreply.github.com>
2026-02-18 18:50:49 +0800

    ✨ Add Node.js parsing-only entry point

--- src/archive.ts
+++ src/archive.ts
@@ -130,8 +130,8 @@ class Archive {
 	 * @return {Blob}
 	 */
 	getBlob(url: string, mimeType?: string): Promise<Blob> | undefined {
-		const decodededUrl = window.decodeURIComponent(url.substr(1)); // Remove first slash
-		const entry = this.zip!.file(decodededUrl);
+		const decodedUrl = decodeURIComponent(url.substr(1)); // Remove first slash
+		const entry = this.zip!.file(decodedUrl);
 
 		if(entry) {
 			mimeType = mimeType || mime.lookup(entry.name);
@@ -149,8 +149,8 @@ class Archive {
 	 * @return text content
 	 */
 	getText(url: string, _encoding?: string): Promise<string> | undefined {
-		const decodededUrl = window.decodeURIComponent(url.substr(1)); // Remove first slash
-		const entry = this.zip!.file(decodededUrl);
+		const decodedUrl = decodeURIComponent(url.substr(1)); // Remove first slash
+		const entry = this.zip!.file(decodedUrl);
 
 		if(entry) {
 			return entry.async("string").then(function(text: string) {
@@ -167,8 +167,8 @@ class Archive {
 	 * @return {string} base64 encoded
 	 */
 	getBase64(url: string, mimeType?: string): Promise<string> | undefined {
-		const decodededUrl = window.decodeURIComponent(url.substr(1)); // Remove first slash
-		const entry = this.zip!.file(decodededUrl);
+		const decodedUrl = decodeURIComponent(url.substr(1)); // Remove first slash
+		const entry = this.zip!.file(decodedUrl);
 
 		if(entry) {
 			mimeType = mimeType || mime.lookup(entry.name);
@@ -187,7 +187,7 @@ class Archive {
 	 */
 	createUrl(url: string, options?: { base64?: boolean }): Promise<string> {
 		const deferred = new defer<string>();
-		const _URL = window.URL || window.webkitURL || window.mozURL;
+		const _URL = typeof window !== "undefined" ? (window.URL || window.webkitURL || window.mozURL) : URL;
 		let tempUrl;
 		let response;
 		const useBase64 = options && options.base64;
@@ -242,15 +242,16 @@ class Archive {
 	 * @param  {string} url url of the item in the archive
 	 */
 	revokeUrl(url: string): void {
-		const _URL = window.URL || window.webkitURL || window.mozURL;
+		const _URL = typeof window !== "undefined" ? (window.URL || window.webkitURL || window.mozURL) : URL;
 		const fromCache = this.urlCache[url];
 		if(fromCache) _URL.revokeObjectURL(fromCache);
 	}
 
 	destroy(): void {
-		const _URL = window.URL || window.webkitURL || window.mozURL;
-		for (const fromCache in this.urlCache) {
-			_URL.revokeObjectURL(fromCache);
+		const _URL = typeof window !== "undefined" ? (window.URL || window.webkitURL || window.mozURL) : URL;
+		for (const key in this.urlCache) {
+			const cachedUrl = this.urlCache[key];
+			if (cachedUrl) _URL.revokeObjectURL(cachedUrl);
 		}
 		this.zip = undefined;
 		this.urlCache = {};
